32456e264846a651fcb36119ea49f9dda33a4dfa
Libenzi Pascal <pascal.libenzi@sokube.ch>
2020-12-13 20:30:42 +0100

    cleanup (move obsolete dockerfiles)

--- employee-quarkus/build-mvn-docker.sh
+++ employee-quarkus/build-mvn-docker.sh
@@ -2,14 +2,9 @@ export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-lts-java11-19.3
 export JAVA_HOME=$GRAALVM_HOME
 export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-lts-java11-19.3.3/Contents/Home/bin:"$PATH"
 
+#jvm standard build
 ./mvnw clean install
 docker build . -f src/main/docker/Dockerfile.jvm -t quarkus-employee-jvm:0.0.1-SNAPSHOT
 
-#./mvnw clean install -Pnative -Dquarkus.native.container-build=true
-#docker build . -f src/main/docker/Dockerfile.native -t quarkus-employee-native:0.0.1-SNAPSHOT
-
-#docker build . -f src/main/docker/Dockerfile.native.alpine -t quarkus-employee-native:0.0.1-SNAPSHOT
-
+# multi stage from scratch build
 docker build . -f src/main/docker/Dockerfile.scratch.native -t quarkus-employee-native:0.0.1-SNAPSHOT-scratch
-
-#docker build . -f src/main/docker/Dockerfile.multi.alpine.native -t quarkus-employee-native:0.0.1-SNAPSHOT-ko-unknown-host
