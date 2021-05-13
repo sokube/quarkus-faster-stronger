40d63e954f8ee4e94b94c602cdb857749e357ee2
Libenzi Pascal <pascal.libenzi@sokube.ch>
2021-05-13 13:38:55 +0200

    reactive begins

---
+++ employee-quarkus/src/main/java/ch/sokube/quarkus/competency/RactiveResource.java
@@ -0,0 +1,35 @@
+package ch.sokube.quarkus.competency;
+
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.time.Duration;
+import java.util.*;
+
+
+@Path("reactive")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
+public class RactiveResource {
+
+    @GET
+    @RestSseElementType(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("randomflux/{iterations}/{duration}")
+    public Multi<String> getRandom(int iterations, int duration) {
+        return Multi.createFrom().ticks().every(Duration.ofMillis(duration))
+                .onItem()
+                .transform(n -> "hello - "+ n + ": " + new Random().nextInt())
+                .transform().byTakingFirstItems(iterations)
+                .onOverflow().buffer(250)
+                .emitOn(Infrastructure.getDefaultExecutor())
+                ;
+    }
+
+}
---
+++ employee-sb/src/main/java/ch/sokube/employee/resources/ReactiveController.java
@@ -0,0 +1,29 @@
+package ch.sokube.employee.resources;
+
+import org.reactivestreams.Publisher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
+import reactor.util.retry.Retry;
+
+import javax.websocket.server.PathParam;
+import java.time.Duration;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/reactive")
+public class ReactiveController {
+
+    @GetMapping("randomflux/{iterations}/{duration}")
+    public Flux<String> getRandom(@PathVariable Integer iterations, @PathVariable Integer duration) {
+        return Flux.fromArray(new String[iterations])
+                .interval(Duration.ofMillis(duration))
+                .onBackpressureBuffer(250)
+                .take(iterations)
+                .map(n -> "hello - " + n + ": " + new Random().nextInt());
+    }
+
+}
